3aef46286b115334a58150079c4e83ebc0b4ee2e
Ádám Nagy <nagyadi14@gmail.com>
2020-11-07 00:37:23 +0100

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mariadb:3306;dbname=junction',
+    'username' => 'junction',
+    'password' => 'Kecske69',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
