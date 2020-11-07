34281d5c7afd418b9835447ae604888d25fc9a31
andrasdenes <andresch.denes@gmail.com>
2020-11-07 22:42:50 +0100

    rm datepicker, submit from merge

--- views/one-day/index.php
+++ views/one-day/index.php
@@ -38,20 +38,6 @@ $form = ActiveForm::begin([
     }
 ?>
 
-?>
-
-<h1>Hellóka One Day</h1>
-
-    <?= DatePicker::widget([
-        'id' => 'one-day-date',
-        'name' => 'select-date',
-        'type' => DatePicker::TYPE_INPUT,
-        'pluginOptions' => [
-            'autoclose' => true,
-            'format' => 'yyyy-mm-dd'
-        ]
-    ]) ?>
-<button type="submit" class="btn btn-primary" onclick="oneDay.postDate()">Gec</button>
 <canvas id="canvas" width="700" height="400"></canvas>
 
 <div id="noticontainer">
